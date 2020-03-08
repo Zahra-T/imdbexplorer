111c54e8125c03b2a95435e09cb309fdc03897a3
Zahra-T <zahratehraninasab@gmail.com>
2020-03-08 21:57:25 +0330

    add print method to Movie class

--- imdbexplorer/movie.py
+++ imdbexplorer/movie.py
@@ -2,8 +2,8 @@
 
 class Movie:
 
-    def __init__(self, name, director, rate, description):
-        self.name = name
+    def __init__(self, title, director, rate, description):
+        self.title = title
         self.director = director
         self.rate = rate
         self.description = description
@@ -11,6 +11,13 @@ class Movie:
     def setindex(self, index):
         self.index = index
 
-    def setlink(imdblink):
+    def setlink(self, imdblink):
         self.imdblink = imdblink
 
+    def print(self):
+        print(f'{self.index}{self.title}')
+        print(f'Rate: {self.rate}')
+        print(f'Director: {self.director}')
+        print(f'Description:{self.description}')
+        print(f'imdb page:{self.imdblink}')
+        print()
