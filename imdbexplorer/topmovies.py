499530663fccc57bb9c81d31e00af2db17c27ea1
Zahra-T <zahratehraninasab@gmail.com>
2020-03-08 19:29:23 +0330

    top 100 movies

---
+++ imdbexplorer/topmovies.py
@@ -0,0 +1,28 @@
+import requests
+from bs4 import BeautifulSoup
+
+imdburl = 'https://www.imdb.com'
+url = 'https://www.imdb.com/list/ls068082370/?sort=user_rating,desc&st_dt=&mode=detail&page=1'
+page = requests.get(url)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+results = soup.find(id='main')
+movies = results.find_all('div', class_='lister-item-content')
+
+for movie in movies:
+    header = movie.find('h3')
+    index = header.find('span', class_='lister-item-index unbold text-primary')
+    title = header.find('a')
+    link = title['href']
+    description = movie.find('p', class_='')
+
+
+    print(f'{index.text}{title.text}')
+    print(f'Description:{description.text}')
+    print(f'imdb page:{imdburl}{link}')
+    print()
+
+
+
+
+
