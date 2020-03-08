be67ae1b914725d196321217e55002b08c6df250
Zahra-T <zahratehraninasab@gmail.com>
2020-03-08 22:00:46 +0330

    printing movie info in a better way

--- imdbexplorer/topmovies.py
+++ imdbexplorer/topmovies.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+from movie import Movie
 
 imdburl = 'https://www.imdb.com'
 url = 'https://www.imdb.com/list/ls068082370/?sort=user_rating,desc&st_dt=&mode=detail&pa0ge=1'
@@ -9,23 +10,16 @@ soup = BeautifulSoup(page.content, 'html.parser')
 results = soup.find(id='main')
 movies = results.find_all('div', class_='lister-item-content')
 
-for movie in movies:
-    header = movie.find('h3')
+for movie_elem in movies:
+    header = movie_elem.find('h3')
     index = header.find('span', class_='lister-item-index unbold text-primary')
     title = header.find('a')
     link = title['href']
-    description = movie.find('p', class_='')
-    director = movie.find_all('p', class_='text-muted text-small')[1].find('a')
-    ratingstar = movie.find('div', class_='ipl-rating-widget').find('span', class_='ipl-rating-star__rating')
+    description = movie_elem.find('p', class_='')
+    director = movie_elem.find_all('p', class_='text-muted text-small')[1].find('a')
+    rate = movie_elem.find('div', class_='ipl-rating-widget').find('span', class_='ipl-rating-star__rating')
 
-    print(f'{index.text}{title.text}')
-    print(f'Rate: {ratingstar.text}')
-    print(f'Director: {director.text}')
-    print(f'Description:{description.text}')
-    print(f'imdb page:{imdburl}{link}')
-    print()
-
-
-
-
-
+    movie = Movie(title.text, director.text, rate.text, description.text)
+    movie.setindex(index.text)
+    movie.setlink(link)
+    movie.print()
