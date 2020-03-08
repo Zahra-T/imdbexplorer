0b0e04549bacb768f2d3e9e549abcf414efae491
Zahra-T <zahratehraninasab@gmail.com>
2020-03-08 21:44:01 +0330

    add movie.py

---
+++ imdbexplorer/movie.py
@@ -0,0 +1,16 @@
+
+
+class Movie:
+
+    def __init__(self, name, director, rate, description):
+        self.name = name
+        self.director = director
+        self.rate = rate
+        self.description = description
+
+    def setindex(self, index):
+        self.index = index
+
+    def setlink(imdblink):
+        self.imdblink = imdblink
+
