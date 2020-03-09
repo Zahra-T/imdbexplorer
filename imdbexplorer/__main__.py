31b6accd0bb9fd9b894616a2acab7ff48c3c68b0
Zahra-T <zahratehraninasab@gmail.com>
2020-03-09 19:54:45 +0330

    change name of topmovies.py to __main__.py and clean the code

---
+++ imdbexplorer/__main__.py
@@ -0,0 +1,41 @@
+import requests
+from bs4 import BeautifulSoup
+from movie import Movie
+
+def topmovies():
+    imdburl = 'https://www.imdb.com'
+    url = 'https://www.imdb.com/list/ls068082370/?sort=user_rating,desc&st_dt=&mode=detail&pa0ge=1'
+    page = requests.get(url)
+
+    soup = BeautifulSoup(page.content, 'html.parser')
+    results = soup.find(id='main')
+    movies_content = results.find_all('div', class_='lister-item-content')
+
+    movies = []
+
+    for movie_elem in movies_content:
+        header = movie_elem.find('h3')
+        index = header.find('span', class_='lister-item-index unbold text-primary')
+        title = header.find('a')
+        link = title['href']
+        description = movie_elem.find('p', class_='')
+        director = movie_elem.find_all('p', class_='text-muted text-small')[1].find('a')
+        rate = movie_elem.find('div', class_='ipl-rating-widget').find('span', class_='ipl-rating-star__rating')
+
+        movie = Movie(title.text, director.text, rate.text, description.text)
+        movie.setindex(index.text)
+        movie.setlink(imdburl+link)
+        movies.append(movie)
+
+    return movies
+
+
+def printmovies(movies):
+    for movie in movies:
+        movie.print()
+
+
+movies = topmovies()
+printmovies(movies)
+
+
